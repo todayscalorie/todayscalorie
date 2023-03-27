e24e4636aae2b5321f7fa2a9584366b1595a9d84
peterlah <peterlah@naver.com>
2023-03-28 00:51:03 +0900

    2023-03-28 초기 환경 구성

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-# todayscalorie
+# 조원
+조공윤님, 김재열님, 최원님, 나상우님
 
 # git global config 방법 - 본인 git 계정 정보 입력
 git config --global user.name "peterlah"
 git config --global user.email "peterlah@naver.com"
 
-# 조원
-조공윤님, 김재열님, 최원님, 나상우님
+# VENV 활성화
+source venv/Scripts/activate
+
+# VENV 비활성화
+deactivate
+
+# pip update
+python.exe -m pip install --upgrade pip
+
+# pip install
+bs4 패키지 -> 웹크롤링 패키지
+requests 패키지 -> web에 GET, POST 요청 위해 사용
+pymongo, dnspython 패키지 -> MongoDB 사용을 위한 패키지
+flask 패키지 -> Flask 서버 개발 용도
+
+pip install bs4 requests pymongo dnspython flask
+
+# 키관리 참고 링크
+https://gomguk.tistory.com/44
---
+++ app.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+from pymongo import MongoClient
+from flask import Flask, render_template, request, jsonify
+# import xml.etree.ElementTree as elemTree
+
+# xml 파일 파싱
+with open('keys.xml', 'r') as file: 
+    tree = BeautifulSoup(file, features="html.parser")
+    db_url = tree.find('string[@name="MongoDB_URL"]').text
+
+app = Flask(__name__)
+
+client = MongoClient(f'{db_url}')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    ogimage = soup.select_one('meta[property="og:image"]')['content']
+    ogtitle = soup.select_one('meta[property="og:title"]')['content']
+    ogdesc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc = {
+        'title': ogtitle,
+        'desc': ogdesc,
+        'image': ogimage,
+        'comment': comment_receive
+    }
+
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg': '저장완료!'})
+
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    all_movies = list(db.movies.find({}, {'_id': False}))
+    return jsonify({'result': all_movies})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
